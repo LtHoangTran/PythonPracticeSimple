d386a608f65a9af767b9b116d25cc74462145de0
Tran Le Hoang <49188050+LtHoangTran@users.noreply.github.com>
2020-06-30 11:20:33 +0700

    Update TicTacToe3x3_2.py
    Update for continuous loop at the end

--- TicTacToe3x3_2.py
+++ TicTacToe3x3_2.py
@@ -130,5 +130,11 @@ def main():
     if isBoardFull(board):
         print('Tie Game!')
 
-main()
-    
+while True:
+    answer = input('Do you want to play again? (Y/N)')
+    if answer.lower() == 'y' or answer.lower == 'yes':
+        board = [' ' for x in range(10)]
+        print('-----------------------------------')
+        main()
+    else:
+        break
